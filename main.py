dee2c41f6feed663bf50312a42f83c204e56a389
uzolify <asujjwalbasnyat@gmail.com>
2025-09-01 14:33:53 +0545

    updated the cross origin link

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Allow frontend origin
 app.add_middleware(
    CORSMiddleware,
-   allow_origins = ['https://mnist-digit-predictor.vercel.app/'],
+   allow_origins = ['https://mnist-digit-predictor.vercel.app', "http://localhost:3000", 'http://192.168.0.100:3000'],
    allow_credentials = True,
    allow_methods = ['*'],
    allow_headers = ['*'],
